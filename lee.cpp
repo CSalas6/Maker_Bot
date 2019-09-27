afc3f75fb451b98826eaa0b50ce89f3c623fe52c
CSalas6 <csalas6@mail.csuchico.edu>
2019-09-27 12:45:33 -0700

    Lee-add comments-uptil constructors

--- lee.cpp
+++ lee.cpp
@@ -10,27 +10,29 @@ private:
   Markov_chain Markov;
   Warkov_chain Warkov;
 */
-  // FN: Read in from the file and generate an occurance rate in map form
-  //  for use by Markov, letter comparisons to one-another
+  // FN: Read in from the file and generate an map<char, int> for Makrov's
+  //  occurance_rate variable. (letter to letter comparisons)
   void Lee::populate_m_pre() {
     char pre = file_access.get();
     for (char suf = file_access.get(); file_access.good(); suf = file_access.get()) {
-      { // seperate zone
         m_fill(pre, suf);
         pre = suf;
-      }
     }
   }
   // Fn poulate_m_pre() helper function. Take inputs of "pre" and "suf" to
   // fill in the Markov prefix map accordingly
   void Lee::m_fill(char pre, char suf) {
+      // If "pre" exists in the markov_prefixes map, else create and add it
     if (markov_prefixes[pre].find() != markov_prefixes.end() ){
       Indiv_char temp = markov_prefixes[pre];
+      // If "suf" in the Indiv_char's "suffix" map then add one, else
+      // create and add one it
       if (temp.suffix[suf].find() != temp.suffix.end() ) {
         temp.suffix[suf] += 1;
       } else {
         temp.suffix.insert(std::pair<char, unsigned int>(suf, 1) );
       }
+      // Else continuation of line_26 If
     } else {
       Indiv_str temp;
       temp.designated_char = pre;
@@ -38,39 +40,44 @@ private:
       markov_prefixes.insert(std::pair<char, Indiv_char>(pre, temp));
     }
   }
-  // FN: Read in from the file and generate an occurance rate in map form
-  //  for use by Warkov, word by word comparisons
+  // FN: Read in from the file and generate an map<string, int> for Wakrov's
+  //  occurance_rate variable. (Word to word comparisons)
   void Lee::populate_w_pre() {
     std::string pre = "", suf = "";
     for (char LetterA = file_access.get(); file_access.good(); suf = file_access.get()) {
       suf = suf + LetterA;
+      // If letter at file_access.get is a space then run w_fill
       if (std::isspace(LetterA) ) {
+        // If "pre" has been set, run w_fill
         if (pre != "") {
           w_fill(pre, suf);
         }
         pre = suf;
         suf = "";
       }
-    } // end of For loop
+    }
   }
   // Fn poulate_w_pre() helper function. Take inputs of "pre" and "suf" to
   // fill in the Warkov prefix map accordingly
   void Lee::w_fill(std::string pre, std::string suf) {
+     // If "pre" exists in the markov_prefixes map, else create and add it
     if (warkov_prefixes[pre].find() != warkov_prefixes.end() ){
       Indiv_str temp = warkov_prefixes[pre];
+      // If "suf" in the Indiv_str's "suffix" map then add one, else
+      // create and add one it
       if (temp.suffix[suf].find() != temp.suffix.end() ) {
         temp.suffix[suf] += 1;
       } else {
         temp.suffix.insert(std::pair<std::string, unsigned int>(suf, 1) );
       }
-    } else {
+    } else {  // Else continuation of line_64 If
       Indiv_str temp;
       temp.designated_str = pre;
       temp.suffix.insert(std::pair<std::string, unsigned int>(suf, 1));
       markov_prefixes.insert(std::pair<char, Indiv_str>(pre, temp));
     }
   }
-  // a private access to set the file_access variable
+  // a private access to set the "file_access" variable
   void Lee::set_file_access(std::string in) {
     if ( file_access.open(in, std::ifstream::in) )
   }
@@ -80,15 +87,20 @@ private:
   // Send the refrence of the warkov_prefixes map to Markov
   void Lee::send_to_warkov();
 
-  // constructors
+  // constructors:
+  // Default - does nothing
   Lee::Lee() {}
+  // Populate Markov and Warkov obj in class with given "a" and "b"
   Lee::Lee(Markov_chain * a, Warkov_chain * b) {
     recieve_markov_obj(a);
     recieve_warkov_obj(b);
   }
+  // Populate "file_access" variable with given "a"
   Lee::Lee(std::string a) {
     recieve_file(a);
   }
+  // Populate "file_access" with "a"; Populate Markov and Warkov obj
+  // with "b" and "c" respectively
   Lee::Lee(std::string a, Markov_chain * b, Warkov_chain * c) {
     recieve_file(a);
     recieve_markov_obj(b);
